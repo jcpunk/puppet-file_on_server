a2603a555af920d2795e25bc01edab5b28154c0e
Pat Riehecky <riehecky@fnal.gov>
2016-02-29 13:26:26 -0600

    Initial build

---
+++ lib/puppet/parser/functions/file_on_server.rb
@@ -0,0 +1,30 @@
+#
+# file_on_server.rb
+#
+
+module Puppet::Parser::Functions
+  newfunction(:file_on_server, :type => :rvalue, :doc => <<-EOS
+This function returns the content of a file found on the puppet
+master.
+
+*Examples:*
+
+    file_on_server('/secrets/myfile')
+
+Would return whatever was contained within /secrets/myfile on
+the puppet master
+    EOS
+  ) do |arguments|
+
+    if (arguments.size != 1) then
+      raise(Puppet::ParseError, "file_on_server(): Wrong number of arguments "+
+        "given #{arguments.size} for 1")
+    end
+
+    contents = File.open('path-to-file.tar.gz', 'rb') { |f| f.read }
+
+    return contents
+  end
+end
+
+# vim: set ts=2 sw=2 et :
